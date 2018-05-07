f1e230f0c9f6c735a024f474f50a742e72025ffc
erkin97 <matkaziev.97@gmail.com>
2018-05-07 15:04:47 +0600

    timerhandler, testing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/yanzay/tbot"
 )
@@ -13,5 +15,20 @@ func main() {
 		log.Fatal(err)
 	}
 	bot.Handle("/answer", "Hello, Master Erkin")
+	bot.HandleFunc("/timer {seconds}", timerHandler)
 	bot.ListenAndServe()
+}
+
+func timerHandler(m *tbot.Message) {
+	// m.Vars contains all variables, parsed during routing
+	secondsStr := m.Vars["seconds"]
+	// Convert string variable to integer seconds value
+	seconds, err := strconv.Atoi(secondsStr)
+	if err != nil {
+		m.Reply("Invalid number of seconds")
+		return
+	}
+	m.Replyf("Timer for %d seconds started", seconds)
+	time.Sleep(time.Duration(seconds) * time.Second)
+	m.Reply("Time out!")
 }
